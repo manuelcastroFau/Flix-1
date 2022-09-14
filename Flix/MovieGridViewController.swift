340dc04479f4cb05caca4bab1735e71c07cd0d70
Manuel Castro <mhcd1984@gmail.com>
2022-09-13 22:41:51 -0400

    Changes in code and Gif added

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 import AlamofireImage
 
-class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate   {
+    
     
     
     
@@ -33,6 +34,7 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         
         let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
         layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
         
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
@@ -92,5 +94,10 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return movies.count
     }
+    
+    
+    
+    
+    
 
 }
